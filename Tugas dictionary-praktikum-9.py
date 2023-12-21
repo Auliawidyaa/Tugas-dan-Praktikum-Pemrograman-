8ea4d9795ab0b3cfb86b5d733919039338c3bc21
Auliawidyaa <153715726+Auliawidyaa@users.noreply.github.com>
2023-12-21 10:31:53 +0800

    Add files via upload

---
+++ Tugas dictionary-praktikum-9.py
@@ -0,0 +1,17 @@
+biodata = {'nama'             : 'Widya Putri Auliya Amin',
+                   'nim'                : '231031048',
+                   'prodi'              : 'Sistem Informasi',
+                   'hobi'               : 'Badminton',
+                   'tanggal_lahir' : '08 Agustus 2005',
+                   'tempat_lahir'  : 'MUR',
+                   'kelas'              : 'SI-23B',
+                   'kuliah'             : 'Institut Teknologi BJ Habibie',
+}
+                   
+print(biodata)
+print('\n')
+print('Nama :',biodata['nama'])
+print('Nim  :',biodata['nim'])
+print('Prodi:',biodata['prodi'])
+print(biodata['nama'],'Tempat tanggal lahir',biodata['tempat_lahir'],biodata['tanggal_lahir'],'Berkuliah di',biodata['kuliah'],'Kelas',biodata['kelas'],'Hobi',biodata['hobi'])
+                   
